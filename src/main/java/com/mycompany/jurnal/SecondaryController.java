29ae53ca407b10ad11f8b2707f8a7f09c21b18cf
Ilya Nasurllaev <ilyaevgenievi4@mail.ru>
2021-10-07 04:07:14 +0300

    added course change when hovering over the button

--- src/main/java/com/mycompany/jurnal/SecondaryController.java
+++ src/main/java/com/mycompany/jurnal/SecondaryController.java
@@ -1,30 +1,49 @@
 package com.mycompany.jurnal;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class SecondaryController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
 
     @FXML
     private TextField loginFild;
 
     @FXML
-    private PasswordField passwordField;
+    private TextField loginFild1;
 
     @FXML
-    private Button loginBtn;
+    private TextField loginFild2;
 
     @FXML
-    private Button signupBtn;
+    private TextField loginFild3;
 
-   // @FXML
-//    void switchToPrimary(ActionEvent event) throws IOException {
-//        App.setRoot("Primary");
-//    }
+    @FXML
+    private Button backBtn;
 
+    @FXML
+    void back(ActionEvent event) throws IOException {
+        App.setRoot("Primary");
+    }
+    
+    @FXML
+    void styleForDef(MouseEvent event) {
+        backBtn.setStyle("-fx-background-color: white");
+    }
 
+    @FXML
+    void styleForEntered(MouseEvent event) {
+        backBtn.setStyle("-fx-background-color: gray");
+    }    
 }
